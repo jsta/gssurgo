7e82e7a0ada912075a564800d29a668466c19e1c
jsta <stachel2@msu.edu>
2018-08-07 14:37:08 -0400

    shrink_bbox to get reasonable example tif

--- tests/get_county_bbox.R
+++ tests/get_county_bbox.R
@@ -8,4 +8,12 @@ county_test <- filter(county_sf, state == "michigan", county == "ingham")
 
 county_test <- st_transform(county_test, crs = 42303)
 
-st_bbox(county_test)
+bbox <- st_bbox(county_test)
+
+shrink_bbox <- function(bbox, fact = 2){
+  bbox[3] <- bbox[1] + ((bbox[3] - bbox[1]) / fact)
+  bbox[4] <- bbox[2] + ((bbox[4] - bbox[2]) / fact)
+  bbox
+}
+
+cat(as.vector(shrink_bbox(bbox)[c(3, 1, 2, 4)]))
