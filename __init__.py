df4e2f68ef95bb0e9b036a725da621596143bb13
jsta <stachel2@msu.edu>
2018-08-14 18:00:52 -0400

    package infracstructure

---
+++ __init__.py
@@ -0,0 +1,1 @@
+name = "gssurgo"
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gssurgo",
+    version="0.0.1",
+    author="Joseph Stachelek",
+    author_email="stachel2@msu.edu",
+    description="Python toolbox enabling an open source gSSURGO workflow",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jsta/gSSURGO",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
