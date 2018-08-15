e4e9418e8ced2576acc27d1c15678e1a01e09128
jsta <stachel2@msu.edu>
2018-08-15 16:29:51 -0400

    trying to call arcpy 2

--- bin/__extract_gssurgo_tif__.py
+++ bin/__extract_gssurgo_tif__.py
@@ -2,8 +2,6 @@ import sys
 try:
     import arcpy
 except:
-    print('could not import arcpy')
-
-    # C:\Python27\ArcGIS10.3\python.exe pull_ssurgo_tif.py gSSURGO_MI.gdb\MapunitRaster_10m tifs\MI.tif    
+    print('could not import arcpy')    
 
     arcpy.CopyRaster_management(sys.argv[1], sys.argv[2])
