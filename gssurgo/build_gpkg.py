a2adb3211c5bac629f1eb76b1a57f36e0c897720
jsta <stachel2@msu.edu>
2018-08-15 22:03:36 -0400

    debug permission error

--- gssurgo/build_gpkg.py
+++ gssurgo/build_gpkg.py
@@ -1,6 +1,6 @@
 import gdal
 
 def build_gpkg(in_gdb, out_gpkg):
-    src_ds = gdal.Open(in_gdb)
+    src_ds = gdal.OpenEx(in_gdb)
     ds = gdal.VectorTranslate(out_gpkg, srcDS = src_ds, format = "GPKG")
     del ds
