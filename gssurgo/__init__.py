48cd6139363fb1d93cf65fdff16a00a0c9380cd5
jsta <stachel2@msu.edu>
2018-08-15 21:59:13 -0400

    export build_gpkg

--- gssurgo/__init__.py
+++ gssurgo/__init__.py
@@ -2,5 +2,6 @@
 # -*- coding: utf-8 -*-
 from .viz_numeric_output import viz_numeric_output
 from .query_gpkg import query_gpkg
+from .build_gpkg import build_gpkg
 
 __all__ = (['viz_numeric_output', 'query_gpkg', 'build_gpkg'])
