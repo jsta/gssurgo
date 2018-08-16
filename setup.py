f84b6cb0595c7ef91c557326691ffcc6ea267b48
jsta <stachel2@msu.edu>
2018-08-16 15:03:41 -0400

    expose states gpkg

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jsta/gssurgo",
     scripts=["bin/extract_gssurgo_tif"],    
+    include_package_data=True,
     packages=setuptools.find_packages(exclude=['tests']),
     classifiers=(
         "Programming Language :: Python :: 3",
