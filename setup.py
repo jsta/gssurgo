8f4057471e9eb8146624f0a97bcdef70d92ea6f5
jsta <stachel2@msu.edu>
2019-05-14 14:36:26 -0400

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gssurgo",
-    version="0.0.4",
+    version="0.0.5",
     author="Joseph Stachelek",
     author_email="stachel2@msu.edu",
     description="Python toolbox enabling an open source gSSURGO workflow",
